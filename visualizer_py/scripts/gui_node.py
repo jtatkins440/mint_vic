5d17fa5d66457a6eb8d41ebd8bbff0099962fda1
Divya Prakash <51917130+tumultousRamen@users.noreply.github.com>
2023-11-27 13:16:12 -0700

    Updated visualizer node

--- visualizer_py/scripts/gui_node.py
+++ visualizer_py/scripts/gui_node.py
@@ -3,15 +3,17 @@ import rospy
 import numpy as np
 from std_msgs.msg import Float64MultiArray
 import matplotlib.pyplot as plt
-from ...srv import *
+from visualizer_py.srv import *
 from scipy import interpolate
-
+from geometry_msgs.msg import PoseStamped
 
 # Global variables
 targetXYold = np.zeros(2)
 targetXY = np.zeros(2)
-endEffectorXY = np.zeros(2)
+endEffectorXY = np.array([[0.0], [0.0]])
 targets = np.zeros((6, 2))
+initial_pose = np.array([0.0,-0.4231, 0.7589])
+
 
 def targetXYold_callback(msg):
     global targetXYold
@@ -22,8 +24,10 @@ def targetXY_callback(msg):
     targetXY = np.array(msg.data)
 
 def endEffectorXY_callback(msg):
-    global endEffectorXY
-    endEffectorXY = np.array(msg.data)
+    current_pose_msg = msg.pose.position
+    abs_pose = np.array([current_pose_msg.x, current_pose_msg.y, current_pose_msg.z]) - initial_pose
+    endEffectorXY[0][0] = abs_pose[2]
+    endEffectorXY[1][0] = abs_pose[0]
 '''
 def targets_callback(msg):
     global targets
@@ -34,7 +38,7 @@ def targets_callback(msg):
 def update_targets(req):
     global targets
     targets = np.column_stack((req.x, req.y))
-    return UpdateTargetsResponse(success=True, message="Targets Updated")
+    return UpdateTargetsResponse(success=True)
 
 
 def visualize():
@@ -42,17 +46,18 @@ def visualize():
     plt.xlim(-0.18, 0.18)
     plt.ylim(-0.18, 0.18)
 
-    print(f"Targets received are: {targets}")
+    # print(f"Targets received are: {targets}")
+    print(f"End Effector Pose: {endEffectorXY}")
 
-    '''# Draw targets
+    # Draw targets
     for target in targets:
-        plt.scatter(target[0], target[1], s=100, c='blue', alpha=0.5)
+        plt.scatter(target[0], target[1], s=200, c='blue', alpha=0.5)
 
     # Draw current target
-    plt.scatter(targetXY[0], targetXY[1], s=100, c='red', alpha=0.5)
+    plt.scatter(targetXY[0], targetXY[1], s=200, c='orange', alpha=0.5, edgecolors='black')
 
     # Draw end effector
-    plt.scatter(endEffectorXY[0], endEffectorXY[1], s=50, c='red')
+    plt.scatter(endEffectorXY[0][0], endEffectorXY[1][0], s=50, c='red')
 
     # Prepare and evaluate the spline
     if len(targets) > 1:
@@ -61,8 +66,8 @@ def visualize():
         out = interpolate.splev(unew, tck)
         plt.plot(out[0], out[1], 'k-', lw=2)
 
-    plt.pause(0.001)
-    '''
+    plt.pause(0.0001)
+    
 
 
 def main():
@@ -70,7 +75,8 @@ def main():
 
     rospy.Subscriber('/PreviousTargets', Float64MultiArray, targetXYold_callback)
     rospy.Subscriber('/CurrentTargets', Float64MultiArray, targetXY_callback)
-    rospy.Subscriber('/CurrentEndEffectorPose', Float64MultiArray, endEffectorXY_callback)
+    rospy.Subscriber('/iiwa/ee_pose', PoseStamped, endEffectorXY_callback)
+    # rospy.Subscriber('/iiwa/j6_pose_custom', Float64MultiArray, endEffectorXY_callback) # --> For simulation
     # rospy.Subscriber('/TrialTargets', Float64MultiArray, targets_callback)
     # Initialize the service
     service = rospy.Service('update_targets', UpdateTargets, update_targets)
@@ -83,7 +89,7 @@ def main():
     d_r = 0.015  # Radius of each target point
     u_r = 0.005  # Radius of end-effector position
 
-    rate = rospy.Rate(10)  # 10 Hz
+    rate = rospy.Rate(100)  # 10 Hz
     plt.ion()
     plt.show()
 
