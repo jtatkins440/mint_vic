2812c915590410e514a332cce35f67dcf9af3998
Divya Prakash <51917130+tumultousRamen@users.noreply.github.com>
2023-11-28 12:52:23 -0700

    updated data logger

--- trial_data_logger/scripts/data_logger_new.py
+++ trial_data_logger/scripts/data_logger_new.py
@@ -3,8 +3,9 @@
 import rospy
 import os
 import h5py
+import time
 from sensor_msgs.msg import JointState
-from std_srvs.srv import Trigger
+from std_srvs.srv import Trigger, TriggerResponse
 from trial_data_logger.srv import *
 from std_msgs.msg import Float64MultiArray
 from motion_intention.srv import *
@@ -14,6 +15,12 @@ from geometry_msgs.msg import PoseStamped, WrenchStamped, TwistStamped
 class TrialDataLogger:
 
     def __init__(self):
+        # File handle for logging
+        self.file_handle = None
+        self.data_group = None
+
+        self.start_time = None
+
         # ROS Node Initialization
         rospy.init_node('trial_data_logger')
 
@@ -32,7 +39,7 @@ class TrialDataLogger:
             # Desired EndEffector Pose
             '/iiwa/ee_pose': rospy.Subscriber('/iiwa/ee_pose', PoseStamped, self.callback),
             # Current End Effector Pose
-            '/iiwa/ee_pose_custom': rospy.Subscriber('/iiwa/ee_pose_custom', Float64MultiArray, self.callback),
+            '/iiwa/ee_pose_custom': rospy.Subscriber('/iiwa/j6_pose_custom', Float64MultiArray, self.callback),
             # End Effector Velocity
             '/iiwa/ee_vel': rospy.Subscriber('/iiwa/ee_vel', TwistStamped, self.callback),
             # End Effector Acceleration
@@ -51,9 +58,7 @@ class TrialDataLogger:
             '/current_stiffness': rospy.Subscriber('/current_stiffness', Float64MultiArray, self.callback)
         }
 
-        # File handle for logging
-        self.file_handle = None
-        self.data_group = None
+
 
     def handle_init_logger(self, req):
         res = InitLoggerResponse()
@@ -102,6 +107,7 @@ class TrialDataLogger:
         
         res.success = True
         res.message = "Started Logging Successfully"
+        self.start_time = time.time()
         return res
 
     def handle_stop_logging(self, req):
@@ -110,6 +116,7 @@ class TrialDataLogger:
             self.file_handle.close()
             self.file_handle = None
             self.data_group = None
+            self.start_time = None
             res.message = "Stopped Logging Successfully"
             res.success = True
 
@@ -120,41 +127,49 @@ class TrialDataLogger:
         return res
 
     def callback(self, msg):
-        if self.data_group:
-            # Use the topic name to create a unique dataset name
-            topic_name = msg._connection_header['topic']
-            dataset_name = topic_name.replace('/', '_').lstrip(
-                '_')  # Clean up the topic name to be used as a dataset name
+        
+        if self.start_time is not None:
+            elapsed_time = time.time() - self.start_time
+            if self.data_group:
+                # Use the topic name to create a unique dataset name
+                topic_name = msg._connection_header['topic']
+                dataset_name = topic_name.replace('/', '_').lstrip(
+                    '_')  # Clean up the topic name to be used as a dataset name
 
-            # Prepare the data based on the message type
-            if isinstance(msg, Float64MultiArray):
-                data_to_append = list(msg.data)
-            elif isinstance(msg, PoseStamped):
-                data_to_append = [msg.pose.position.x, msg.pose.position.y, msg.pose.position.z,
-                                  msg.pose.orientation.x, msg.pose.orientation.y,
-                                  msg.pose.orientation.z, msg.pose.orientation.w]
-            elif isinstance(msg, TwistStamped):
-                data_to_append = [msg.twist.linear.x, msg.twist.linear.y, msg.twist.linear.z,
-                                  msg.twist.angular.x, msg.twist.angular.y, msg.twist.angular.z]
-            elif isinstance(msg, WrenchStamped):
-                data_to_append = [msg.wrench.force.x, msg.wrench.force.y, msg.wrench.force.z,
-                                  msg.wrench.torque.x, msg.wrench.torque.y, msg.wrench.torque.z]
-            elif isinstance(msg, JointState):
-                data_to_append = list(msg.position)
-            else:
-                rospy.logwarn(f"Unsupported message type: {type(msg)}")
-                return
+                # Prepare the data based on the message type
+                if isinstance(msg, Float64MultiArray):
+                    data_to_append = [elapsed_time]+list(msg.data)
+                elif isinstance(msg, PoseStamped):
+                    data_to_append = [elapsed_time]+[msg.pose.position.x, msg.pose.position.y, msg.pose.position.z,
+                                                    msg.pose.orientation.x, msg.pose.orientation.y,
+                                                    msg.pose.orientation.z, msg.pose.orientation.w]
+                elif isinstance(msg, TwistStamped):
+                    data_to_append = [elapsed_time]+[msg.twist.linear.x, msg.twist.linear.y, msg.twist.linear.z,
+                                                    msg.twist.angular.x, msg.twist.angular.y, msg.twist.angular.z]
+                elif isinstance(msg, WrenchStamped):
+                    data_to_append = [elapsed_time]+[msg.wrench.force.x, msg.wrench.force.y, msg.wrench.force.z,
+                                                    msg.wrench.torque.x, msg.wrench.torque.y, msg.wrench.torque.z]
+                elif isinstance(msg, JointState):
+                    data_to_append = [elapsed_time]+list(msg.position)
+                else:
+                    rospy.logwarn(f"Unsupported message type: {type(msg)}")
+                    return
 
-            # Check if the dataset exists, if so, append, otherwise create
-            if dataset_name in self.data_group:
-                # Append to the dataset
-                dataset = self.data_group[dataset_name]
-                dataset.resize((dataset.shape[0] + 1, len(data_to_append)))
-                dataset[-1] = data_to_append
-            else:
-                # Create a new dataset
-                self.data_group.create_dataset(dataset_name, data=[data_to_append],
-                                               maxshape=(None, len(data_to_append)))
+                try:
+                    # Check if the dataset exists, if so, append, otherwise create
+                    if dataset_name in self.data_group:
+                    # Append to the dataset
+                        dataset = self.data_group[dataset_name]
+                        dataset.resize((dataset.shape[0] + 1, len(data_to_append)))
+                        dataset[-1] = data_to_append
+                    else:
+                        # Create a new dataset
+                        self.data_group.create_dataset(dataset_name, data=[data_to_append],
+                                       maxshape=(None, len(data_to_append)))
+                except KeyError as e:
+                    rospy.logerr("Failed to access or create dataset: {}. Error: {}".format(dataset_name, e))
+        else:
+            rospy.loginfo("Logging hasn't started yet")
 
 
 if __name__ == '__main__':
