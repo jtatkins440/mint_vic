62a692b405138277e1796720cdb56ae2f560dff5
Divya Prakash <51917130+tumultousRamen@users.noreply.github.com>
2024-01-03 15:23:54 -0700

    Latest MInt Node

---
+++ motion_intention/src/history_handler_node.cpp
@@ -0,0 +1,266 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/TwistStamped.h"
+#include "geometry_msgs/Pose.h"
+#include "std_msgs/Float64MultiArray.h"
+#include "std_msgs/MultiArrayDimension.h"
+#include "motion_intention/HistoryStamped.h"
+#include "motion_intention/SetInt.h"
+#include <sstream>
+#include <Eigen/Dense>
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <deque>
+#include <chrono>
+#include <mutex>
+#include <thread>
+#include "iir/Iir.h" // filtering library
+
+class HistoryHandlerWrapper{
+	public:
+	HistoryHandlerWrapper(){
+		nh = ros::NodeHandle("history_handler");
+		nh.getParam("state_dim", state_dim);
+		nh.getParam("seq_length", seq_length);
+		nh.getParam("history_rate", history_rate);
+		history_dt = 1.0 / history_rate;
+		history_dim = state_dim * 3;
+		pos_vel_dim = state_dim * 2;
+		spinner_thread_num = 3; // one spinner per subscriber
+		main_loop_duration = std::chrono::duration<double>(history_dt);
+
+		for (int i = 0; i < filter_dim; i++){
+			filter_array[i].setup(filter_sampling_freq, filter_cutoff_freq);
+		}
+		
+		sub_pose = nh.subscribe("/iiwa/ee_pose", 1, &HistoryHandlerWrapper::callbackPose, this);
+		sub_vel = nh.subscribe("/iiwa/ee_vel", 1, &HistoryHandlerWrapper::callbackVelocity, this);
+		sub_acc = nh.subscribe("/iiwa/ee_acc", 1, &HistoryHandlerWrapper::callbackAcceleration, this);
+		pub_hist = nh.advertise<motion_intention::HistoryStamped>("/ee_history", 1);
+
+		b_state_history_ready = false;
+
+		hist_msg.history.layout.dim = {std_msgs::MultiArrayDimension(), std_msgs::MultiArrayDimension()};
+		// dim[0] is the vertical dimension of your matrix
+        hist_msg.history.layout.dim[0].label = "pos_vel_acc";
+        hist_msg.history.layout.dim[0].size = history_dim;
+        hist_msg.history.layout.dim[0].stride = history_dim * seq_length;
+        // dim[1] is the horizontal dimension of your matrix
+        hist_msg.history.layout.dim[1].label = "samples";
+        hist_msg.history.layout.dim[1].size = seq_length;
+        hist_msg.history.layout.dim[1].stride = seq_length;
+
+		std::string logstring = "history_handler: recieved state_dim: " + std::to_string(state_dim) + "\nseq_length: " + std::to_string(seq_length) + "\nhistory_rate: " + std::to_string(history_rate);
+		ROS_INFO(logstring.c_str());
+		pos_vel_state_vector = std::vector<double>(state_dim * 2, 0);
+		state_vector = std::vector<double>(state_dim * 3, 0);
+		last_state_vector = std::vector<double>(state_dim * 3, 0);
+		flat_history_vector = std::vector<double>(history_dim * seq_length, 0); //should be a 125 * 6 vector of zeros
+	};
+
+	// attributes
+	int state_dim;
+	int pos_vel_dim;
+	int seq_length;
+	double history_rate;
+	double history_dt;
+	int history_dim;
+	std::vector<double> current_position_vec;
+	std::vector<double> current_velocity_vec;
+	std::vector<double> current_acceleration_vec;
+	std::vector<double> pos_vel_state_vector;
+	std::vector<double> state_vector;
+	std::vector<double> last_state_vector;
+	std::vector<double> filtered_state_vector;
+	std::deque<std::vector<double>> state_history_vector;
+	bool b_state_history_ready;
+	std::mutex mtx_pos; // mutex guards for the async threads updating the values
+	std::mutex mtx_vel;
+	std::mutex mtx_acc;
+	std::chrono::duration<double> main_loop_duration;
+
+	// filter attributes, values needs to be known at compile time! look for a workaround that doesn't involve hardcoding...
+	static constexpr int filter_order {2};
+	static constexpr double filter_cutoff_freq {40};
+	static constexpr double filter_sampling_freq {200};
+	static constexpr int filter_dim {6}; // should be 3 * state_dim in order tow work correctly
+	Iir::Butterworth::LowPass<filter_order> filter_array[filter_dim];
+
+	// ros attributes
+	ros::NodeHandle nh;
+	ros::Subscriber sub_pose;
+	ros::Subscriber sub_vel;
+	ros::Subscriber sub_acc;
+	ros::Publisher pub_hist;
+	motion_intention::HistoryStamped hist_msg;
+	std::vector<double> flat_history_vector;
+	int spinner_thread_num;
+
+	// methods
+	void callbackPose(const geometry_msgs::PoseStamped::ConstPtr& msg);
+	void callbackVelocity(const geometry_msgs::TwistStamped::ConstPtr& msg);
+	void callbackAcceleration(const geometry_msgs::TwistStamped::ConstPtr& msg);
+
+	void filterStateVector();
+	void updateDeque();
+	void publishHistoryArray();
+
+	void waitRemainingLoopTime(std::chrono::time_point<std::chrono::steady_clock> start_time, std::chrono::duration<double> desired_duration);
+	bool isStateVectorUpdated();
+	void mainLoop();
+};
+
+void HistoryHandlerWrapper::callbackPose(const geometry_msgs::PoseStamped::ConstPtr& msg){
+	//std::lock_guard<std::mutex> guard(mtx_pos);
+    state_vector[0] = msg->pose.position.x;
+	state_vector[1] = msg->pose.position.z;
+	//current_position_vec.clear();
+    //current_position_vec = {msg->pose.position.x, msg->pose.position.z};
+};
+
+void HistoryHandlerWrapper::callbackVelocity(const geometry_msgs::TwistStamped::ConstPtr& msg){
+	//std::lock_guard<std::mutex> guard(mtx_vel);
+	state_vector[2] = msg->twist.linear.x;
+	state_vector[3] = msg->twist.linear.z;
+	//current_velocity_vec.clear();
+	//current_velocity_vec = {msg->twist.linear.x, msg->twist.linear.z};
+};
+
+void HistoryHandlerWrapper::callbackAcceleration(const geometry_msgs::TwistStamped::ConstPtr& msg){
+	//std::lock_guard<std::mutex> guard(mtx_acc); // should unlock when function ends
+	state_vector[4] = msg->twist.linear.x;
+	state_vector[5] = msg->twist.linear.z;
+	//current_acceleration_vec.clear();
+	//current_acceleration_vec = {msg->twist.linear.x, msg->twist.linear.z};
+};
+
+bool HistoryHandlerWrapper::isStateVectorUpdated(){
+	for (int i = 0; i < state_vector.size(); i++){
+		if (state_vector[i] == last_state_vector[i]){
+			return false;
+		}
+	}
+	last_state_vector.clear();
+	last_state_vector = state_vector;
+	return true; // only happens if none of the elements in state_vector are the same as last_state_vector
+};
+
+void HistoryHandlerWrapper::filterStateVector(){
+	/*
+	std::string logstring = "history_handler: state_vector: \n";
+	for (int i = 0; i < state_vector.size(); i++){
+		logstring = logstring + " " + std::to_string(state_vector[i]);
+	}
+	ROS_INFO(logstring.c_str());
+	*/
+	filtered_state_vector.clear();
+	for (int i = 0; i < filter_dim; i++){
+		filtered_state_vector.push_back(filter_array[i].filter(state_vector[i]));
+		//filtered_state_vector.push_back(state_vector[i]);
+	}
+	
+	/*
+	logstring = "history_handler: filtered_state_vector: \n";
+	for (int i = 0; i < filtered_state_vector.size(); i++){
+		logstring = logstring + " " + std::to_string(filtered_state_vector[i]);
+	}
+	ROS_INFO(logstring.c_str());
+	*/
+
+	pos_vel_state_vector.clear();
+	for (int i = 0; i < pos_vel_dim; i++){
+		pos_vel_state_vector.push_back(filtered_state_vector[i]);
+	}
+};
+
+void HistoryHandlerWrapper::updateDeque(){
+	if (isStateVectorUpdated()){
+		filterStateVector(); // updates filtered_state_vector with current filtered values
+		/*
+		if (seq_length == state_history_vector.size()){
+			state_history_vector.pop_front();
+			b_state_history_ready = true;
+		}
+		else if (seq_length < state_history_vector.size()){ // this case shouldn't happen but just in case we should make sure there's only seq_length elements in there
+			while (seq_length < state_history_vector.size()){
+				state_history_vector.pop_front();
+			}
+			b_state_history_ready = true;
+		}
+		*/
+
+		if (seq_length <= state_history_vector.size()){
+			state_history_vector.pop_front();
+			b_state_history_ready = true;
+		}
+		else {
+			b_state_history_ready = false; // it's defaulted to false but it's probably good to make sure just in case
+		}
+
+		state_history_vector.push_back(filtered_state_vector);
+	}
+};
+
+void HistoryHandlerWrapper::publishHistoryArray(){
+		// update the header's timestamp and the current state
+		hist_msg.header.stamp = ros::Time::now(); 
+		hist_msg.state = pos_vel_state_vector; // .data() ?
+
+		// clear the only history array and put the new one in there
+		hist_msg.history.data.clear();
+		//flat_history_vector.clear(); // does clearing change the size? if so, don't do this
+		for (int i = 0; i < history_dim; i++){
+			for (int j = 0; j < seq_length; j++){
+				flat_history_vector[i * seq_length + j] = state_history_vector[j][i]; // should overwrite every element quickly, want to fill it in 'row by row'
+			}
+		}
+		hist_msg.history.data = flat_history_vector;
+		
+		/*std::string logstring = "history_handler: flat_history_vector: \n";
+		for (int i = 0; i < flat_history_vector.size(); i++){
+			logstring = logstring + " " + std::to_string(flat_history_vector[i]);
+		}
+		ROS_INFO(logstring.c_str());
+		*/
+		pub_hist.publish(hist_msg);
+};
+
+void HistoryHandlerWrapper::waitRemainingLoopTime(std::chrono::time_point<std::chrono::steady_clock> start_time, std::chrono::duration<double> desired_duration){
+	std::chrono::duration<double> diff = std::chrono::steady_clock::now() - start_time; 
+	while (diff.count() < desired_duration.count()){
+		diff = std::chrono::steady_clock::now() - start_time;
+		
+	}
+};
+
+// THEN THIS
+void HistoryHandlerWrapper::mainLoop(){
+	// if enough time has past, update the deque then publish
+	ros::AsyncSpinner spinner(spinner_thread_num);
+	spinner.start();
+
+	std::chrono::time_point<std::chrono::steady_clock> start_time = std::chrono::steady_clock::now();
+	waitRemainingLoopTime(start_time, main_loop_duration);
+
+	ROS_INFO("history_handler: Starting main loop...");
+	while (ros::ok()){
+		start_time = std::chrono::steady_clock::now();
+		updateDeque();
+		if (b_state_history_ready){
+			publishHistoryArray();
+		}
+		waitRemainingLoopTime(start_time, main_loop_duration);
+
+	}
+	spinner.stop();
+	ros::waitForShutdown();
+};
+
+int main(int argc, char **argv){
+	ros::init(argc, argv, "history_handler");
+	HistoryHandlerWrapper history_handler;
+	history_handler.mainLoop();
+	return 0;
+}
--- motion_intention/src/motion_intention_node.cpp
+++ motion_intention/src/motion_intention_node.cpp
@@ -50,14 +50,13 @@ class MIntNodeWrapper{
     		mint_circ = CircleFitWrapper(hyperparam_weights_path);
 
 			nh.param<double>("sample_time", sample_time, 0.005);
-			nh.param<double>("allowable_time", allowable_time_tolerance, 0.0005);
 			nh.param<int>("inference_rate", inference_rate, 500);
 			nh.param<int>("pose_deque_min_size", pose_deque_min_size, 3);
 			nh.param<int>("state_dim", state_dim, 2);
 			nh.param<int>("seq_length", seq_length, 125);
 			nh.param<int>("mint_state_dim", mint_state_dim, 4);
 			nh.param<int>("cfit_state_dim", cfit_state_dim, 2);
-
+			nh.param<bool>("swap_xy_axis", b_swap_axis, false);
 			input_deque_seq_length = mintnet.input_seq_length;
 			time_start = std::chrono::steady_clock::now();
 
@@ -71,9 +70,10 @@ class MIntNodeWrapper{
 		};
 
 		void mainLoop();
-		void dequeHandler();
-		void startDequeHandler();
+		//void dequeHandler();
+		//void startDequeHandler();
 		void fitHandler();
+		void waitRemainingLoopTime(std::chrono::time_point<std::chrono::steady_clock> start_time, std::chrono::duration<double> desired_duration);
 
 		//ros::Subscriber sub;
 		ros::Subscriber sub_hist;
@@ -93,25 +93,23 @@ class MIntNodeWrapper{
 		Eigen::ArrayXXf cfit_input_array;
 		int pose_deque_min_size;
 		double sample_time;
-		double allowable_time_tolerance;
 		int inference_rate;
 		int position_size;
 		int state_dim;
 		int seq_length;
 		int mint_state_dim;
 		int cfit_state_dim;
+		bool b_swap_axis;
 
 		bool b_deque_ready = false; // flag for toggling if deque is full or not
 		bool b_mint_ready = false; // flag for toggling if mint method is fitted and ready to give predictions
 
-
 		std::string model_weights_path, hyperparam_weights_path;
 		ros::ServiceServer srv_set_mint_type;
-		
 
-		void subscriberCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+		//void subscriberCallback(const geometry_msgs::PoseStamped::ConstPtr& msg);
 		void subscriberHistoryCallback(const motion_intention::HistoryStamped::ConstPtr& msg);
-		void updateDeque(Eigen::ArrayXf new_pose, float dt);
+		//void updateDeque(Eigen::ArrayXf new_pose, float dt);
 		bool setMotionIntentType(motion_intention::SetInt::Request &req, motion_intention::SetInt::Response &res);
 
 	private:
@@ -119,7 +117,6 @@ class MIntNodeWrapper{
 };
 
 bool MIntNodeWrapper::setMotionIntentType(motion_intention::SetInt::Request &req, motion_intention::SetInt::Response &res){
-	
     
     std::string out_string;
 	std::string type_name;
@@ -140,7 +137,6 @@ bool MIntNodeWrapper::setMotionIntentType(motion_intention::SetInt::Request &req
 		return res.success; // gets out of 
 	}
 
-	
 	res.success = true;
 	fit_type = req.data;
     out_string = "motion_intention class fitting type set to " + type_name + "!!!";
@@ -148,6 +144,7 @@ bool MIntNodeWrapper::setMotionIntentType(motion_intention::SetInt::Request &req
     return res.success;
 };
 
+/*
 void MIntNodeWrapper::subscriberCallback(const geometry_msgs::PoseStamped::ConstPtr& msg){
 	geometry_msgs::Point position_new;
 	position_new = msg -> pose.position;
@@ -159,6 +156,7 @@ void MIntNodeWrapper::subscriberCallback(const geometry_msgs::PoseStamped::Const
 
 	return;
 };
+*/
 
 void MIntNodeWrapper::subscriberHistoryCallback(const motion_intention::HistoryStamped::ConstPtr& msg){
 	int rows = msg->history.layout.dim[0].size;
@@ -168,19 +166,28 @@ void MIntNodeWrapper::subscriberHistoryCallback(const motion_intention::HistoryS
   	Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> hist_matrix(data.data(), rows, cols);
 
 	Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> full_hist_matrix = (hist_matrix.cast <float> ());
-	cfit_input_array = full_hist_matrix.block(0,0,cfit_state_dim,cols).eval();
-	mint_input_array = full_hist_matrix.block(2,0,mint_state_dim,cols).eval();
-
-	
 
 	Eigen::Matrix<float, 4, 1> current_state_temp;
-	current_state_temp << (float) msg->state[0], (float) msg->state[1], (float) msg->state[2], (float) msg->state[3];
+	
+
+	if (b_swap_axis){
+		// swap x and y channels to account for the frame transformation
+		Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> full_hist_matrix_temp = full_hist_matrix;
+		int axis_blocks = rows / 2;
+		for (int i = 0; i < axis_blocks; i++){
+			full_hist_matrix.block(i * state_dim, 0, 1, cols) = full_hist_matrix_temp.block(i * state_dim + 1, 0, 1, cols);
+			full_hist_matrix.block(i * state_dim + 1, 0, 1, cols) = full_hist_matrix_temp.block(i * state_dim, 0, 1, cols);
+		}
+		current_state_temp << (float) msg->state[1], (float) msg->state[0], (float) msg->state[3], (float) msg->state[2];
+	} 
+	else{
+		current_state_temp << (float) msg->state[0], (float) msg->state[1], (float) msg->state[2], (float) msg->state[3];
+	}
+
+	cfit_input_array = full_hist_matrix.block(0,0,cfit_state_dim,cols).eval();
+	mint_input_array = full_hist_matrix.block(2,0,mint_state_dim,cols).eval();
 	current_state.swap(current_state_temp);
 	b_deque_ready = true;
-
-	//std::cout << "mint_input_array = " << std::endl << mint_input_array << std::endl;
-	//std::cout << "cfit_input_array = " << std::endl << cfit_input_array << std::endl;
-	//std::cout << "current_state = " << std::endl << current_state << std::endl;
 };
 
 void MIntNodeWrapper::fitHandler(){
@@ -190,19 +197,9 @@ void MIntNodeWrapper::fitHandler(){
 	std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(time_current - time_start);
 	if (sample_time <= time_span.count()){
 		// if enough time has passed, update the deque and refit the spine
-		//std::cout << "Updating mintnet, time_span.count(): " << time_span.count() << std::endl;
 		if (b_deque_ready){
-			//ROS_WARN("MInt: in fitHandler, in b_deque_ready, before .fit() functions near 186:");
-			//Eigen::Array<float, 4, 1> current_state;
-			//Eigen::Array<float, 4, 1> current_vel_acc;
-			//current_vel_acc = input_array[input_array.size() - 1];
-			//current_state << current_position(0), current_position(1), current_vel_acc(0), current_vel_acc(1);
-			//std::cout << "dequeHandler, current_state: " << current_state << std::endl;
-			//ROS_WARN("MInt: in fitHandler, in b_deque_ready, before mintnet.fit():");
 			mintnet.fit(current_state, mint_input_array); // expects [current_position; current_velocity] for first argument!
-			//ROS_WARN("MInt: in fitHandler, in b_deque_ready, before mint_line.fit():");
 			mint_line.fit(current_state, cfit_input_array); 
-			//ROS_WARN("MInt: in fitHandler, in b_deque_ready, before mint_circ.fit():");
 			mint_circ.fit(current_state, cfit_input_array); 
 			b_mint_ready = true;
 		}
@@ -210,28 +207,24 @@ void MIntNodeWrapper::fitHandler(){
 	}
 };
 
+/*
 void MIntNodeWrapper::dequeHandler(){
 	std::chrono::steady_clock::time_point time_current = std::chrono::steady_clock::now();
 	bool update_deque_flag = true;
-
 
 	time_current = std::chrono::steady_clock::now();
 	std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(time_current - time_start);
 	if (sample_time <= time_span.count()){
 		// if enough time has passed, update the deque and refit the spine
-		//Eigen::ArrayXf pose_new(mintnet.output_chn_size, 1);
 		Eigen::Array<float, 2, 1> pose_new;
 		pose_new << current_position(0), current_position(1); // x and y position only!
-		//std::cout << "Before updateDeque..." << std::endl;
 		std::cout << "time_span.count(): " << time_span.count() << std::endl;
 		updateDeque(pose_new, time_span.count());
-		//std::cout << "After updateDeque..." << std::endl;
 		if (b_deque_ready){
 			Eigen::Array<float, 4, 1> current_state;
 			Eigen::Array<float, 4, 1> current_vel_acc;
 			current_vel_acc = input_deque[input_deque.size() - 1];
 			current_state << current_position(0), current_position(1), current_vel_acc(0), current_vel_acc(1);
-			//std::cout << "dequeHandler, current_state: " << current_state << std::endl;
 
 			std::cout << std::endl;
 			mintnet.fit(current_state, input_deque); // expects [current_position; current_velocity] for first argument!
@@ -241,15 +234,12 @@ void MIntNodeWrapper::dequeHandler(){
 		time_start = std::chrono::steady_clock::now();
 	}
 };
+*/
 
+/*
 // this assumes the deque will be given inputs of global positions. Ideally we'd just have velocity and acceleration inputs from the admittance controller.
 void MIntNodeWrapper::updateDeque(Eigen::ArrayXf new_pose, float dt){
-	// if mintnet
 
-	// fill in deque
-
-
-	//std::cout << "Before pose_deque.size()" << std::endl;
 	if (pose_deque.size() >= input_deque_seq_length) {
 		pose_deque.pop_front(); // if we don't seperate these two conditions then pose_deque would be longer than input_deque. Not a big problem though, merge later if needed.
 	}
@@ -262,56 +252,35 @@ void MIntNodeWrapper::updateDeque(Eigen::ArrayXf new_pose, float dt){
 
 	pose_deque.push_back(new_pose);
 	if (pose_deque.size() > pose_deque_min_size){
-		//std::cout << "pose_deque[pose_deque.size() - 1]: " << pose_deque[pose_deque.size() - 1] << std::endl;
-		//std::cout << "pose_deque[pose_deque.size() - 2]: " << pose_deque[pose_deque.size() - 2] << std::endl;
 		Eigen::Array<float, 2, 1> vel_est;
 		vel_est << (pose_deque[pose_deque.size() - 1] - pose_deque[pose_deque.size() - 2]) / dt;
 		Eigen::Array<float, 2, 1> acc_est;
 		acc_est << (vel_est - (pose_deque[pose_deque.size() - 2] - pose_deque[pose_deque.size() - 3]) / dt) / dt;
 		Eigen::Array<float, 4, 1> new_input;
-		//std::cout << "vel_est: " << vel_est << " acc_est: " << acc_est << std::endl;
 		new_input << vel_est, acc_est;
-		//std::cout << "new_input: " << new_input << std::endl;
 		input_deque.push_back(new_input);
 	}
-	
-	// if circlefit
-
-	// if linearfit
 
 	return; 
+};
+*/
+
+void MIntNodeWrapper::waitRemainingLoopTime(std::chrono::time_point<std::chrono::steady_clock> start_time, std::chrono::duration<double> desired_duration){
+	std::chrono::duration<double> diff = std::chrono::steady_clock::now() - start_time; 
+	while (diff.count() < desired_duration.count()){
+		diff = std::chrono::steady_clock::now() - start_time; 
+	}
 };
 
 void MIntNodeWrapper::mainLoop() {
 	
 	ros::Rate r(inference_rate);
-
-	//static tf2_ros::TransformBroadcaster br;
-
-	/*
-	geometry_msgs::TransformStamped transformStamped;
-	transformStamped.header.frame_id = "world";
-	transformStamped.child_frame_id = "ee_eq";
-	transformStamped.header.stamp = ros::Time::now();
-	transformStamped.transform.rotation.x = 0.0;
-	transformStamped.transform.rotation.y = 0.0;
-	transformStamped.transform.rotation.z = 0.0;
-	transformStamped.transform.rotation.w = 1.0;
-	*/
-	
-	//ROS_INFO("MInt: before while (ros::ok()):");
 	while (ros::ok()){
 		geometry_msgs::PoseStamped pose_s; // empty posestamped message
-
-		//dequeHandler();
-		//ROS_INFO("MInt: before fitHandler():");
-		//ROS_WARN("MInt: before fitHandler():");
 		fitHandler();
-		//Eigen::ArrayXf eq_pose = Eigen::ArrayXf::Zero(2,1);
 		Eigen::ArrayXf eq_pose = Eigen::ArrayXf::Zero(2,1);
 		if (b_mint_ready){
 			// update empty message with fitted value
-			//ROS_WARN("MInt: before if(fit_type):");
 			if (fit_type == 0){
 				eq_pose = mintnet.getEquilibriumPoint();
 			}
@@ -325,19 +294,21 @@ void MIntNodeWrapper::mainLoop() {
 				ROS_WARN("MInt: Invalid fit_type!");
 			}
 			
-			std::cout << "MInt: eq_pose: " << eq_pose << " for fit_type: " << fit_type << "." << std::endl;
-			//ROS_WARN("MInt: before setting pose_s:");
-			pose_s.pose.position.x = eq_pose(0);
-			pose_s.pose.position.z = eq_pose(1);
+			if (b_swap_axis){
+				pose_s.pose.position.x = eq_pose(1);
+				pose_s.pose.position.z = eq_pose(0);
+			} 
+			else{
+				pose_s.pose.position.x = eq_pose(0);
+				pose_s.pose.position.z = eq_pose(1);
+			}
+			
 			pose_s.pose.orientation.w = 1.0;
 			pose_s.header.frame_id = "ee_eq";
 			pose_s.header.stamp = ros::Time::now();
-			//std::cout << "Got new eq_pose! It's: "<< eq_pose << std::endl;
 
 		}
-		//ROS_INFO("MInt: before publishing pose_s:");
 		pub.publish(pose_s);
-		//br.sendTransform(transformStamped);
 
 		ros::spinOnce();
 		r.sleep();
@@ -348,7 +319,5 @@ void MIntNodeWrapper::mainLoop() {
 int main(int argc, char **argv){
 	ros::init(argc, argv, "motion_intent");
 	MIntNodeWrapper minty;
-
 	minty.mainLoop();
-	//deque_handler_thread.join();
 }
